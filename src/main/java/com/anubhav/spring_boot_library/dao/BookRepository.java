6d92255247479774de7453d23cb0d22d8cf685f8
Anubhav Ranjan <60106478+Mr-AB007@users.noreply.github.com>
2024-11-28 00:51:08 +0530

    Initial commit

---
+++ src/main/java/com/anubhav/spring_boot_library/dao/BookRepository.java
@@ -0,0 +1,7 @@
+package com.anubhav.spring_boot_library.dao;
+
+import com.anubhav.spring_boot_library.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
